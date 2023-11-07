f34cc0a4ed671a2ea1bc247296c87693520f28ff
AlexBringh <115021574+AlexBringh@users.noreply.github.com>
2023-11-07 20:57:15 +0100

    commit

--- Equation_Of_Motion/Symbolic/Numerical_Integration.py
+++ Equation_Of_Motion/Symbolic/Numerical_Integration.py
@@ -1,9 +1,10 @@
+import numpy as np
+from scipy.integrate import solve_ivp
+import matplotlib.pyplot as plt
+
 import Equation_Of_Motion as eom
 
 
 """
     Runge Kutta numerical integration method
 """
-
-def Runge_Kutta ():
-    pass
